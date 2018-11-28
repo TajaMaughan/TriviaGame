7282550f1628aea30205283db83b63d67fa78cdc
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2018-11-27 18:15:26 -0700

    psuedocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+//create an object to hold the questions and answers.
+const questions = [{
+    question: "",
+    options: {},
+    answer: "",
+}]
+//create functions that allow the quiz to run, show results, etc.
+//The quiz function will run on page load,
+function theQuiz() {
+//have it bring up the questions with corresponding answers.
+//make sure it has radios before each answer. (should make it so that only one can be chosen)
+//create a timer for the whole quiz.
+// when the timer lapses or the user submits the quiz, show results
+}
+//on the results page show number of questions answered correctly, incorrectly, or not answered.
+//give the user the option to restart the quiz.
+function showResults() {
+    
+}
