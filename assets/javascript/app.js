77778fab15e455b4778e23bb689a5423581abc6e
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2018-12-01 21:03:46 -0700

    works up through checking results

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,50 +50,94 @@ const questions = [
         },
         answer: "a"
     }
-]
+];
 
-var timer = 60;
+var timer = 6;
 var intervalId;
-var answers = [];
-
+var answers;
+var output;
+var values;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
+//timer function
 function timerStart() {
     clearInterval(intervalId);
     intervalId = setInterval(countDown, 1000);
 }
-
+//decrement timer function
 function countDown() {
     timer--;
     $("#timer").html("<p>" + timer + "</p>")
+    if (timer == 0) {
+        clearInterval(intervalId);
+        $("#quizHide").hide();
+        //showResults();
+        getValues();
+    }
 }
-
+//show quiz function
 function showQuiz(question, quizContainer) {
-    var output = [];
-    var answers;
+    output = [];
+    answers;
     for (i = 0; i < questions.length; i++) {
         answers = [];
         for (letter in questions[i].options) {
-            answers.push('<label>' + '<input type="radio" name="question' + i + ' value="' + letter + '">' + letter + ': ' + questions[i].options[letter] + '</label><br>')
+            answers.push('<label>' + '<input type="radio" name="answer' + [i] + '"' + 'value="' + letter + '">' + letter + ': ' + questions[i].options[letter] + '</label><br>')
         };
-    
+
         output.push('<div class="question">' + questions[i].question + '</div>' + '<div class="answers">' + answers.join('') + '</div>');
     }
     output = output.join('');
     $("#quiz").append(output);
 }
+
+function getValues() {
+    values = [];
+    for (j = 0; j < answers.length + 1; j++) {
+        var selectedAnswer = $("input[name=answer" + [j] + "]:checked").val()
+        values.push(selectedAnswer);
+
+    }
+    console.log(values);
+}
+
+function checkValues() {
+    for (k = 0; k < questions.length; k++) {
+        if (questions[k].answer === values[k]) {
+            correctAnswers++;
+        } else if (values[k] === undefined) {
+            unanswered++;
+        } else {
+            incorrectAnswers++;
+        }
+    }
+}
+
+/*function showResults() {
+    for (j = 0; j < answers.length; j++) {
+        //answers = 
+        var input = answers.push(document.querySelector("input[name='" + j + "'"))
+    
+        //var value = $(".answer").val();
+        console.log(input);
+    }
+}*/
+
+//on the results page show number of questions answered correctly, incorrectly, or not answered.
+//give the user the option to restart the quiz.
+
+
 //start button to begin quiz
-function startButton() {
+$("body").on("click", "#startButton", function startButton() {
+    $("#startButton").hide();
     //starts timer
     timerStart();
     countDown();
+    //shows quiz    
     showQuiz();
-
-    //shows quiz
-    //have it bring up the questions with corresponding answers.
-    //make sure it has radios before each answer. (should make it so that only one can be chosen)
     // when the timer lapses or the user submits the quiz, show results
-}
-//on the results page show number of questions answered correctly, incorrectly, or not answered.
-//give the user the option to restart the quiz.
-function showResults() {
 
-}
+})
+
+$("body").on("click", "#submitButton", showResults)
